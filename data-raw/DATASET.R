7c1541e3a1c409f01efb67cec2498c817b2efd3d
Max Lemes <maxvlemes@gmail.com>
2024-07-21 16:55:42 -0300

    funcoes  resumo

--- data-raw/DATASET.R
+++
@@ -1,8 +0,0 @@
-## code to prepare `DATASET` dataset goes here
-
-source('data-raw/orig_cad.R')
-source('data-raw/orig_anexoII.R')
-source('data-raw/orig_listas.R')
-source('data-raw/orig_topicos.R')
-
-usethis::use_data(orig_cad, anexoII, listas, topicos, internal = TRUE, overwrite = TRUE)
