befe076b8ee5d1f8df9a8b5c1015f2aa05bec6b1
Max Lemes <maxvlemes@gmail.com>
2024-07-22 20:06:19 -0300

    funcoes  resumo

---
+++ scripts/nada.R
@@ -0,0 +1,5 @@
+JOAO VITOR DE PINA ADORNO DE PAIVA <joaopina@discente.ufg.br>
+  MARIA CLARA DUTRA COSTA <maria.dutra@discente.ufg.br>
+   
+      
+THIAGO PEREIRA DE LACERDA <thiago_lacerda@discente.ufg.br>
